4d12969b22e382d6627aae68873842412d846ace
Nick Senger <dev@nsenger.com>
2018-03-05 15:14:10 -0800

    chore: Removes unnecessary js files

--- demos/ts-with-dojo/app/main.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", "./widgets/AppBody"], function (require, exports, AppBody_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    new AppBody_1.default({}, "viewDiv");
-});
